1d2f68dcbac9c9d9196d10fdc6039d6f3a4a32f7
romandviski <romandviski@mail.ru>
2016-08-14 19:13:27 +0300

    добавлен класс HorizontalLine

---
+++ HorizontalLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSnake
+{
+    class HorizontalLine
+    {
+        List<Point> PList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            PList = new List<Point>();
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                PList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in PList)
+            {
+                p.Draw();
+            }
+        }
+
+
+
+    }
+}
