bff531090acaacf8ef10dae1faf7348571ffaee5
romandviski <romandviski@mail.ru>
2016-08-14 13:39:48 +0300

    класс поинт

--- Program.cs
+++ Program.cs
@@ -11,13 +11,17 @@ namespace ConsoleSnake
         static void Main(string[] args)
         {
 
-            // Console.WriteLine("Hello world");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
-
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
 
 
@@ -27,11 +31,6 @@ namespace ConsoleSnake
 
 
             Console.ReadLine();
-        }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
