f64b0e5be640211c6d73829d99f0cefd60377a3b
romandviski <romandviski@mail.ru>
2016-08-14 13:26:31 +0300

    коммит

--- Program.cs
+++ Program.cs
@@ -10,8 +10,28 @@ namespace ConsoleSnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+
+            // Console.WriteLine("Hello world");
+
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
+
+
+
+
+
+
+
+
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
