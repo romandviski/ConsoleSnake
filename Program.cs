d9ad30ad8f3a07c1aa835739b12562d192bd615b
romandviski <romandviski@mail.ru>
2016-08-14 18:54:18 +0300

    Добавлена работа со списками

--- Program.cs
+++ Program.cs
@@ -11,18 +11,28 @@ namespace ConsoleSnake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            List<char> CharList = new List<char>();
+            CharList.Add('*');
+            CharList.Add('#');
+            CharList.Add('@');
+            CharList.Add('$');
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+                Point p1 = new Point(1, 3, CharList[0]);
+                //p1.Draw();
+                Point p2 = new Point(4, 5, CharList[1]);
+                Point p3 = new Point(6, 8, CharList[2]);
+                Point p4 = new Point(10, 13, CharList[3]);
 
+            List<Point> PointList = new List<Point>();
+            PointList.Add(p1);
+            PointList.Add(p2);
+            PointList.Add(p3);
+            PointList.Add(p4);
 
-
-
-
-
-
+            foreach(Point i in PointList)
+            {
+                i.Draw();
+            }
 
             Console.ReadLine();
         }
